b8da9caf210f328a0e7a67c477464bdc10fa2bf2
Yovin-gethubs <yovinshayo358@gmail.com>
2025-02-09 17:49:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Forms</title>
+</head>
+<body>
+    <h1>ARUSHA APPARTMENT</h1>
+    <div class="container">
+        <form action="">
+            <div>
+                <label>Name</label>
+                <input type="text" name="name" placeholder="Enter
+                 your name">
+</div>
